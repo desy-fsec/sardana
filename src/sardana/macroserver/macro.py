a7c27972f7a846ce9393369bb3d820ffa799280d
teresa <tnunez@mail.desy.de>
2016-05-27 11:45:57 +0200

    Correcting loggingtofile of list. Cleanup

--- src/sardana/macroserver/macro.py
+++ src/sardana/macroserver/macro.py
@@ -877,10 +877,11 @@ class Macro(Logger):
                 logging_path = self.getEnv("LogMacroPath")
 
         if logging_onoff:
+            msgstr = msg
             try:
-                if type(msg) == list:
-                    msg = ' '.join(msg)
-                Logger.loggingtofile(self, msg, logging_mode, logging_path, *args, **kwargs)
+                if type(msgstr) == list:
+                    msgstr = ' '.join(msgstr)
+                Logger.loggingtofile(self, msgstr, logging_mode, logging_path, *args, **kwargs)
             except:
                 self.warning("Not able to write log file. Check if the path for logging %s exist." % logging_path)
 
